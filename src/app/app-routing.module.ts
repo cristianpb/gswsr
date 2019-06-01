972c3738105e2e7c8515b3639ced07eda381782a
cristianpb <felipebrokate@gmail.com>
2019-06-01 19:33:33 +0200

    Add tweets component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TweetsComponent } from './tweets/tweets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tweets', pathMatch: 'full' },
+  { path: 'tweets', component: TweetsComponent },
+  { path: '**', component: TweetsComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
