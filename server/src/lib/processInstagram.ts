c3df4fda22e9e38e39b2f716e644baeb7b6317bb
cristianpb <felipebrokate@gmail.com>
2019-06-05 23:46:38 +0200

    Save hashtags and mentions for instagram

--- server/src/lib/processInstagram.ts
+++ server/src/lib/processInstagram.ts
@@ -6,14 +6,48 @@ import { postInsta } from '../entities/postInsta';
 import axios from 'axios';
 
 export class ProcessInstagram {
+
+  static saveHashtag = async (db: Db, post: postInsta, reg: RegExp, annotationType: string) => {
+    const hashtags = await post.body.match(reg);
+    const updateVal: SetTags = await {};
+    if (annotationType === 'mention') {
+      updateVal.mentions = hashtags;
+    }
+    if (annotationType === 'hashtag') {
+      updateVal.hashtags = hashtags;
+    }
+    if (hashtags) {
+      console.log('Text: ', post.body, '\n', annotationType, ': ', hashtags);
+      if (hashtags.length > 0) {
+        try {
+          await db.collection('documents').findOneAndUpdate(
+            {id: post.id},
+            { $set: updateVal}
+          );
+          console.log(`updated post ${post.id}`);
+        } catch (err) {
+          console.log('Post update error', err);
+        }
+      }
+    } else {
+      console.log(yellow(`Text: ${post.body} \nNo ${annotationType}`));
+    }
+  }
+
+  static saveMetadata = async (db: Db, post: postInsta) => {
+    const msg3 = ProcessInstagram.saveHashtag(db, post, /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/gm, 'hashtag');
+    const msg4 = ProcessInstagram.saveHashtag(db, post, /@(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/gm, 'mention');
+  }
+
   static savePosts = async (db: Db, data: rawInstagram) => {
     const post = await new postInsta(data);
     try {
+      await ProcessInstagram.saveMetadata(db, post);
       const restInsert = await db.collection('documents').insertOne(post);
       return green(`Saved ${post.id} ${restInsert.result.ok}`);
     } catch (err) {
       if (err.code === 11000) {
-        const { id } = post
+        const { id } = post;
         delete post.id;
         delete post._id;
         const result: any = await db.collection('documents').findOneAndUpdate(
@@ -43,4 +77,9 @@ export class ProcessInstagram {
     return `Processed posts`;
   }
 
+}
+
+interface SetTags {
+  mentions?: string[];
+  hashtags?: string[];
 }
