376c247fb200381bbb15ab16fe988dc4c720f859
cristianpb <felipebrokate@gmail.com>
2019-06-08 18:04:37 +0200

    Add autorefresh to frontend

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -8,18 +8,34 @@ import { TwitterService } from '../twitter.service';
 })
 export class DocumentsComponent implements OnInit {
   documents: any = [];
+  timer;
   currentPage = 0;
 
   constructor(private twitter: TwitterService) {}
 
   ngOnInit() {
     this.getDocuments();
+    this.timer = setInterval(() => {
+      this.updateDocuments()
+    }, 30000);
+  }
+
+  updateDocuments() {
+    this.twitter.fetchDocuments(0).subscribe((result: any) => {
+      result.data.forEach((item) => {
+        if (this.documents.map(item => item.id).indexOf(item.id) === -1) {
+          this.documents.unshift(item)
+        }
+      })
+    });
   }
 
   getDocuments() {
     this.twitter.fetchDocuments(this.currentPage).subscribe((result: any) => {
       result.data.forEach((item) => {
-        this.documents.push(item)
+        if (this.documents.map(item => item.id).indexOf(item.id) === -1) {
+          this.documents.push(item)
+        }
       })
     });
   }
