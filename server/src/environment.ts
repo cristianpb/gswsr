b0eea05a601b5ab58d24db299e60854ba99b59f5
cristianpb <felipebrokate@gmail.com>
2020-12-04 08:35:15 +0100

    🚚 migrate to mongo atlas

--- server/src/environment.ts
+++ server/src/environment.ts
@@ -1,7 +1,9 @@
-const mlab_username = process.env.MLAB_USERNAME;
-const mlab_password = process.env.MLAB_PASSWORD;
+//const mlab_username = process.env.MLAB_USERNAME;
+//const mlab_password = process.env.MLAB_PASSWORD;
+const atlas_url = process.env.ATLAS_URL;
 
 export const environment = {
-  mongourl:  `mongodb://${mlab_username}:${mlab_password}@ds263876.mlab.com:63876/gswsr`,
+  // mongourl:  `mongodb://${mlab_username}:${mlab_password}@ds263876.mlab.com:63876/gswsr`,
+  mongourl:  `${atlas_url}`,
   mongoDatabase: 'gswsr'
 };
